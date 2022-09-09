3ecad2ffcb2ea6a07f30c292201a2095be4e1a8d
DennyTimmy <denyefa.leader@gmail.com>
2022-09-09 02:39:30 -0700

    alx-low_level_programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- *  * * main - prints the size of various types
- *   *
- *    * * Return: 0 if exited properly, non-zero otherwise
- *     *
- *      *
- *       */
+ * main - Entry point
+ * puts
+ *
+ * Return: Always 0 (Sucess)
+ */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
+return (0);
+
 }
