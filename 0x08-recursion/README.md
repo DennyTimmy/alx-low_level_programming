5c901e32b6456f141403a342d346c3e035fa8e05
DennyTimmy <denyefa.leader@gmail.com>
2022-09-28 14:16:17 -0700

    README.md

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,11 @@
+-> Welcome to the read me file of Project 8.
+
+-> This is a task on recursion while learning the C-programming language
+
+-> It is Expideient to note here that it has always been very interesting when you know what to do and how to do it.
+
+-> All files were uploaded from the terminal
+
+-> All required files were installed and edited with the approved system
+
+-> For this porject i used the VI tool to edit update all files
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,5 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 void _puts_recursion(char *s);
@@ -8,8 +8,10 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int _sqrt_check(int guess, int num);
 int is_prime_number(int n);
+int prime_check(int i, int num);
 int is_palindrome(char *s);
-int wildcmp(char *s1, char *s2);
+int palindrome_check(int index, int revStrInd, char *str);
 
 #endif
