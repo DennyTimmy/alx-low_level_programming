9fc5d0f5a983d95a8a9c64d862ad58b665e0aabd
DennyTimmy <denyefa.leader@gmail.com>
2022-10-31 14:57:56 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,44 +1,45 @@
 #include "main.h"
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <stdlib.h>
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: name of the file to read
- * @letters: number of letters it should read and print
+ * read_textfile - Reads a text file and prints it to the
+ * POSIX standard output
+ * @filename: File name of the archive
+ * @letters: Number of letters stored in the buffer
  *
- * Return: actual number of letters it could read and print
+ * Return: The size of the letters readed and printed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t lenr, lenw;
-	char *buffer;
+	int fd, readData;
+	char *buffer = malloc(sizeof(char) * letters);
+	ssize_t ans;
 
-	if (filename == NULL)
+	if (buffer == NULL || filename == NULL)
 		return (0);
+
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return (0);
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-	{
-		close(fd);
-		return (0);
-	}
-	lenr = read(fd, buffer, letters);
-	close(fd);
-	if (lenr == -1)
 	{
 		free(buffer);
 		return (0);
 	}
-	lenw = write(STDOUT_FILENO, buffer, lenr);
-	free(buffer);
-	if (lenr != lenw)
+	readData = read(fd, buffer, letters);
+	if (readData == -1)
+	{
+		free(buffer);
+		close(fd);
 		return (0);
-	return (lenw);
+	}
+	ans = write(STDOUT_FILENO, buffer, readData);
+	if ((ans == -1) || (ans != readData))
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+	free(buffer);
+	readData = close(fd);
+	if (readData == -1)
+		return (0);
+	return (ans);
 }
